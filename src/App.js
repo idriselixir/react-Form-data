4ef2e766e4885364fb35b29c289a15229b52f039
Idris Noor <idreesnoor150@gmail.com>
2023-11-15 17:26:26 +0300

    updated the styles

--- src/App.js
+++ src/App.js
@@ -1,68 +1,93 @@
-import React, { useState } from 'react';
-import './Form.css';
-
-const Form = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    mobile: '',
-    email: '',
-    dob: '',
-    origin: '',
-    age: '',
-    gender: '',
-    status: ''
-  });
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Do something with the form data
-    console.log(formData);
-  };
-
+import { useState } from "react";
+import './App.css';
+function App() {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [date, setDate] = useState();
+  const [POO, setPOO] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [status, setStatus] = useState("");
   return (
-    <div className="form-container">
-      <h2>Registration Form</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
-
-        <label htmlFor="mobile">Mobile Number:</label>
-        <input type="text" id="mobile" name="mobile" value={formData.mobile} onChange={handleChange} />
-
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
-
-        <label htmlFor="dob">Date of Birth:</label>
-        <input type="date" id="dob" name="dob" value={formData.dob} onChange={handleChange} />
-
-        <label htmlFor="origin">Place of Origin:</label>
-        <input type="text" id="origin" name="origin" value={formData.origin} onChange={handleChange} />
-
-        <label htmlFor="age">Age:</label>
-        <input type="number" id="age" name="age" value={formData.age} onChange={handleChange} />
-
-        <label htmlFor="gender">Gender:</label>
-        <select id="gender" name="gender" value={formData.gender} onChange={handleChange}>
-          <option value="">Select</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-          <option value="other">Other</option>
-        </select>
-
-        <label htmlFor="status">Status:</label>
-        <input type="text" id="status" name="status" value={formData.status} onChange={handleChange} />
-
-        <button type="submit">Submit</button>
-      </form>
+    <div className="App">
+      <h1>Fill the Form</h1>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Number"
+        value={number}
+        onChange={(event) => {
+          setNumber(event.target.value);
+        }}
+      />
+      <input
+        type="Email"
+        placeholder="Email"
+        value={email}
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
+      />
+      <input
+        type="Date"
+        placeholder="D.O.B"
+        value={date}
+        onChange={(event) => {
+          setDate(event.target.value);
+        }}
+      />
+      <input
+        type="Text"
+        placeholder="P.O.O"
+        value={POO}
+        onChange={(event) => {
+          setPOO(event.target.value);
+        }}
+      />
+      <input
+        type="Number"
+        placeholder="Age"
+        value={age}
+        onChange={(event) => {
+          setAge(event.target.value);
+        }}
+      />
+      <input
+        type="Text"
+        placeholder="Gender"
+        value={gender}
+        onChange={(event) => {
+          setGender(event.target.value);
+        }}
+      />
+      <input
+        type="Text"
+        placeholder="Status"
+        value={status}
+        onChange={(event) => {
+          setStatus(event.target.value);
+        }}
+      />
+      <div>
+        <h1>Name:{name}</h1>
+        <h1>Number:{number}</h1>
+        <h1>Email:{email}</h1>
+        <h1>Date:{date}</h1>
+        <h1>P.O.O:{POO}</h1>
+        <h1>Age:{age}</h1>
+        <h1>Gender:{gender}</h1>
+        <h1>Status:{status}</h1>
+      </div>
     </div>
   );
-};
+}
 
-export default Form;
+export default App;
